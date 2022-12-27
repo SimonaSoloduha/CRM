722195fc43efff963aa1c323eef6ee5e33e70d2b
Simona Soloduha <ls460simona@gmail.com>
2022-12-27 15:30:07 +0200

    Update ALLOWED_HOSTS with site and development server URL

--- crm/settings.py
+++ crm/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['123.123.123.123', '127.0.0.1']
+ALLOWED_HOSTS = ['damp-everglades-01529.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
