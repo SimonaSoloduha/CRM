97928908adc16e6f58a8f04f45a25a681007a6c0
Simona Soloduha <ls460simona@gmail.com>
2022-12-30 10:59:44 +0200

    add pagination to logs api

--- logs/views.py
+++ logs/views.py
@@ -3,6 +3,12 @@ from rest_framework.permissions import AllowAny
 
 from logs.models import Log
 from logs.serializers import LogSerializer
+from rest_framework.pagination import PageNumberPagination
+
+
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 5
+    page_size_query_param = 'page_size'
 
 
 class LogViewSet(viewsets.ModelViewSet):
@@ -12,3 +18,4 @@ class LogViewSet(viewsets.ModelViewSet):
     queryset = Log.objects.all().order_by('-created_at')
     serializer_class = LogSerializer
     permission_classes = [AllowAny]
+    pagination_class = StandardResultsSetPagination
