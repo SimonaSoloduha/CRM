7bc204e5e287d87bec7abc383e48e529ea072adf
Simona Soloduha <ls460simona@gmail.com>
2023-01-16 16:47:57 +0200

    add fix to table's

--- crm/settings.py
+++ crm/settings.py
@@ -172,6 +172,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
+
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
