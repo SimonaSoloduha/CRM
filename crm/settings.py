fbac912e3328726b5d2a90c6d77d379521399ebd
Simona Soloduha <ls460simona@gmail.com>
2023-01-16 15:32:55 +0200

    add fix to table's

--- crm/settings.py
+++ crm/settings.py
@@ -159,11 +159,11 @@ LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale')]
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = 'static'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
