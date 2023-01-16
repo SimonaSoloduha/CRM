01626a32e887d1d8cb9213cbf12fa1bce8aca119
Simona Soloduha <ls460simona@gmail.com>
2023-01-16 16:53:47 +0200

    add fix to table's

--- crm/settings.py
+++ crm/settings.py
@@ -160,7 +160,7 @@ LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale')]
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static', '/static')
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
 
