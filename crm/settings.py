4b635ae19eefc26640821974303a3a299628a205
Simona Soloduha <ls460simona@gmail.com>
2023-02-02 14:53:07 +0300

    add fix settings.py

--- crm/settings.py
+++ crm/settings.py
@@ -184,8 +184,8 @@ STATIC_ROOT = '/var/cache/CRM/static/'
 STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
-
+# MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
+MEDIA_ROOT = '/var/opt/CRM/media/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
